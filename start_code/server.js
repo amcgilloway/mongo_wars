dbc41c04d452b8f232195d7fd70f69fec47490cb
Ally McGilloway <alastair.mcgilloway@codeclan.com>
2017-04-18 11:58:13 +0100

    Adds starter code

---
+++ start_code/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var parser = require('body-parser');
+var app = express();
+var path = require("path");
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static('client/build'));
+
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/client/build/index.html');
+
+});
